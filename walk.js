ab77052b718dfc15d80d6efe500e5938b33d0832
blgianini <skaailpvp>
2022-02-14 23:05:02 -0300

    Andar em circuloss

--- walk.js
+++ walk.js
@@ -1,6 +1,7 @@
 const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals: {GoalNear} } = require('mineflayer-pathfinder')
 const bot = mineflayer.createBot({ host: 'localhost', port: 62641, username: 'Walker' })
+const { Vec3 } = require('vec3')
 
 const RANGE_GOAL = 1 
 bot.loadPlugin(pathfinder)
@@ -12,6 +13,7 @@ bot.once('spawn', () =>{
     bot.on('chat', (username, message) =>{
         if (username === bot.username) return
         if (message !== 'come') return
+        console.log(bot.entity.position)
         const target = bot.players[username]?.entity
 
         if(!target){
@@ -21,8 +23,25 @@ bot.once('spawn', () =>{
         const {x: playerX, y: playerY, z: playerZ} = target.position
         
         bot.pathfinder.setMovements(defaultMove)
-        bot.pathfinder.setGoal(new GoalNear(10, 10, 10, RANGE_GOAL))
-        bot.pathfinder.setGoal(new GoalNear(-10, 10, -10, RANGE_GOAL))
+
+        bot.on('time', () =>{
+            bot.pathfinder.setMovements(defaultMove)
+            console.log(bot.entity.position)
+            if(bot.entity.position.x > 0 && bot.entity.position.x < 1 && bot.entity.position.z > 0 && bot.entity.position.z < 1){
+                patrulha(10,10)
+            }else if (bot.entity.position.x > 10 && bot.entity.position.x < 11 && bot.entity.position.z > 10 && bot.entity.position.z < 11){
+                patrulha(10,20)
+            }else if (bot.entity.position.x > 10 && bot.entity.position.x < 11 && bot.entity.position.z > 20 && bot.entity.position.z < 21){
+                patrulha(0,0)
+            }
+        })
+
         
     })
-})
+}) 
+
+
+
+function patrulha(position1, position2){
+    return bot.pathfinder.setGoal(new GoalNear(position1, 10,position2, RANGE_GOAL))
+}
