d9521d0c121319da245e8da9697c4ed84d138e2d
blgianini <skaailpvp>
2022-02-16 21:58:50 -0300

    tentando arar a terra

--- path.js
+++ path.js
@@ -1,7 +1,15 @@
 const mineflayer = require('mineflayer')
+const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
 const { pathfinder, Movements, goals: {GoalNear} } = require('mineflayer-pathfinder')
 const bot = mineflayer.createBot({ host: 'localhost', port: 65393, username: 'Path' })
 const { Vec3 } = require('vec3')
+
+
+let mcData
+bot.on('inject_allowed', () => {
+  mcData = require('minecraft-data')(bot.version)
+})
+
 
 bot.loadPlugin(pathfinder)
 
@@ -29,9 +37,17 @@ bot.on('chat', (username, message) => {
 
             if(bot_x == splits[1] && bot_y == splits[2]){
                 bot.pathfinder.setGoal(new GoalNear(splits[3], 10,splits[4], RANGE_GOAL))
+                const toSow = blockToSow()
+                if (toSow) {
+                   bot.equip(mcData.itemsByName.wheat_seeds.id, 'hand')
+                   bot.placeBlock(toSow, new Vec3(0, 1, 0))
+                }
             }else if(bot_x == splits[3] && bot_y == splits[4]){
+                bot.pathfinder.setGoal(new GoalNear(splits[5], 10,splits[6], RANGE_GOAL))
+            }else if(bot_x == splits[5] && bot_y == splits[6]){
                 bot.pathfinder.setGoal(new GoalNear(splits[1], 10,splits[2], RANGE_GOAL))
             }
+            
         })
 
     }
@@ -39,3 +55,14 @@ bot.on('chat', (username, message) => {
 
 
 
+function blockToSow () {
+    return bot.findBlock({
+      point: bot.entity.position,
+      matching: mcData.blocksByName.farmland.id,
+      maxDistance: 6,
+      useExtraInfo: (block) => {
+        const blockAbove = bot.blockAt(block.position.offset(0, 1, 0))
+        return !blockAbove || blockAbove.type === 0
+      }
+    })
+  }
